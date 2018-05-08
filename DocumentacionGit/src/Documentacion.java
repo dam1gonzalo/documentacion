ce24a6e0190e8e95eb7e57b0c91ec8e7b597c248
Gonzalo Ulecia <paginastown@gmail.com>
2018-05-08 03:13:04 -0700

    VERSION PARA GITHUB

--- DocumentacionGit/src/Documentacion.java
+++ DocumentacionGit/src/Documentacion.java
@@ -1,6 +1,6 @@
 public class Documentacion {
 	public static void main(String[] args){
 		System.out.println("Welcome to Git World");
-		System.out.println("VERSION 2");
+		System.out.println("VERSION EN GITHUB");
 	}
 }
